18611e8b9d57d96a4b902a32fc60993da059f90a
ぷりん <nrs.yuuki@gmail.com>
2018-05-21 15:40:10 +0900

    tset

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+error_log("callback start");
 
+require_once( dirname(__FILE__).DIRECTORY_SEPARATOR."config.php" );
 
-echo "hi";
+// Http request body
+$requests = json_decode(file_get_contents('php://input'));
+
+error_log($requests);
