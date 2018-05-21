2d9c8ce9f85d0995da8165da8c95a71fbae4b16c
ぷりん <nrs.yuuki@gmail.com>
2018-05-21 14:28:28 +0900

    Delete

---
+++ class/BtcExchangeApi.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * BTC為替API
+ * @memo Read here
+ */
+class BtcExchangeApi
+{
+    function __construct()
+    {
+    }
+
+    function GetData()
+    {
+
+        $api_url = "https://public.bitbank.cc/btc_jpy/ticker";
+
+        //curl実行
+        $ch = curl_init($api_url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        ));
+        $json = curl_exec($ch);
+        curl_close($ch);
+
+        $data = json_decode($json);
+
+        return $data->data->last;
+    }
+
+}
+
