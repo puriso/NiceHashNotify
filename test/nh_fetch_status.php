89d8b413424486761186ab4a6e55f398df88e4a6
ぷりん <nrs.yuuki@gmail.com>
2018-05-22 10:15:46 +0900

    test

--- test/nh_fetch_status.php
+++ test/nh_fetch_status.php
@@ -1,5 +1,4 @@
 <?php
-ini_set('display_errors', "On");
 echo "start";
 define("RELATIVE_PATH","..");
 define("CLASS_DIRECTORY",dirname(__FILE__).DIRECTORY_SEPARATOR.RELATIVE_PATH.DIRECTORY_SEPARATOR."class".DIRECTORY_SEPARATOR);
@@ -8,7 +7,7 @@ require_once( RELATIVE_PATH . dirname(__FILE__).DIRECTORY_SEPARATOR.RELATIVE_PAT
 require_once( CLASS_DIRECTORY . "LineApi.php" );
 require_once( CLASS_DIRECTORY . "NiceHashApi.php" );
 
-$NiceHashAPI = new NiceHashAPI();
+$NiceHashAPI     = new NiceHashAPI();
 $nicehash_status = $NiceHashAPI->FetchWorkerStatus();
 
 var_dump($nicehash_status);
