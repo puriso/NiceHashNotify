cfa02c23c9b6a1fc1eac0272c23a7622634ccb28
ぷりん <nrs.yuuki@gmail.com>
2018-05-22 14:49:37 +0900

    init

---
+++ test/nh_fetch_status.php
@@ -0,0 +1,16 @@
+<?php
+ini_set('display_errors', "1");
+echo "start";
+define("RELATIVE_PATH","..");
+define("CLASS_DIRECTORY",dirname(__FILE__).DIRECTORY_SEPARATOR.RELATIVE_PATH.DIRECTORY_SEPARATOR."class".DIRECTORY_SEPARATOR);
+
+require_once( dirname(__FILE__).DIRECTORY_SEPARATOR.RELATIVE_PATH.DIRECTORY_SEPARATOR."config.php" );
+require_once( CLASS_DIRECTORY . "LineApi.php" );
+require_once( CLASS_DIRECTORY . "NiceHashApi.php" );
+require_once( CLASS_DIRECTORY . "BtcExchangeApi.php" );
+
+$NiceHashAPI     = new NiceHashAPI();
+$nicehash_status = $NiceHashAPI->MakeTextForMessage();
+
+print_r($nicehash_status);
+
